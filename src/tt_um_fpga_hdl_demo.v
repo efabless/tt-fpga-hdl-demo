9df396852d2714b87ca93208babb46202e5fa8f1
jeffdi <jeffdi@efabless.com>
2024-01-26 15:05:18 -0800

    mods from Pat

--- src/tt_um_fpga_hdl_demo.v
+++ src/tt_um_fpga_hdl_demo.v
@@ -3,9 +3,6 @@
 module tt_um_fpga_hdl_demo #( parameter MAX_COUNT = 24'd10_000_000 ) (
     input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
     output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
-    input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
-    output wire [7:0] uio_out,  // IOs: Bidirectional Output path
-    output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
     input  wire       ena,      // will go high when the design is enabled
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
@@ -15,9 +12,6 @@ module tt_um_fpga_hdl_demo #( parameter MAX_COUNT = 24'd10_000_000 ) (
     wire [6:0] led_out;
     assign uo_out[6:0] = led_out;
     assign uo_out[7] = 1'b0;
-
-    use bidirectionals as outputs
-    assign uio_oe = 8'b11111111;
 
     // put bottom 8 bits of second counter out on the bidirectional gpio
     // assign uio_out = second_counter[7:0];
