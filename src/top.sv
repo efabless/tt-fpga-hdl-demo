4526d40f06efcde40b7f8401190bef41489f4b48
jeffdi <jeffdi@efabless.com>
2024-01-28 15:35:15 -0800

    add debounce

--- src/top.sv
+++ src/top.sv
@@ -67,7 +67,7 @@ module top(input logic clk, input logic reset, input logic [31:0] cyc_cnt, outpu
    */
 
    // Instantiate the Tiny Tapeout module.
-   tt_um_template tt(.*);
+   my_design tt(.*);
 
    assign passed = top.cyc_cnt > 60;
    assign failed = 1'b0;
@@ -76,7 +76,59 @@ endmodule
 
 
 // Provide a wrapper module to debounce input signals if requested.
+// The Tiny Tapeout top-level module.
+// This simply debounces and synchronizes inputs.
+// Debouncing is based on a counter. A change to any input will only be recognized once ALL inputs
+// are stable for a certain duration. This approach uses a single counter vs. a counter for each
+// bit.
+module tt_um_template (
+    input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
+    output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
+    input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
+    output wire [7:0] uio_out,  // IOs: Bidirectional Output path
+    output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // will go high when the design is enabled
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
+);
+    // Synchronize.
+    logic [17:0] inputs_ff, inputs_sync;
+    always @(posedge clk) begin
+        inputs_ff <= {ui_in, uio_in, ena, rst_n};
+        inputs_sync <= inputs_ff;
+    end
 
+    // Debounce.
+    `define DEBOUNCE_MAX_CNT 8'hff
+    logic [17:0] inputs_candidate, inputs_captured;
+    logic sync_rst_n = inputs_sync[0];
+    logic [7:0] cnt;
+    always @(posedge clk) begin
+        if (!sync_rst_n)
+           cnt <= `DEBOUNCE_MAX_CNT;
+        else if (inputs_sync != inputs_candidate) begin
+           // Inputs changed before stablizing.
+           cnt <= `DEBOUNCE_MAX_CNT;
+           inputs_candidate <= inputs_sync;
+        end
+        else if (cnt > 0)
+           cnt <= cnt - 8'b1;
+        else begin
+           // Cnt == 0. Capture candidate inputs.
+           inputs_captured <= inputs_candidate;
+        end
+    end
+    logic [7:0] clean_ui_in, clean_uio_in;
+    logic clean_ena, clean_rst_n;
+    assign {clean_ui_in, clean_uio_in, clean_ena, clean_rst_n} = inputs_captured;
+
+    my_design my_design (
+        .ui_in(clean_ui_in),
+        .uio_in(clean_uio_in),
+        .ena(clean_ena),
+        .rst_n(clean_rst_n),
+        .*);
+endmodule
 // The above macro expands to multiple lines. We enter a new \SV block to reset line tracking.
 //_\SV
 
@@ -86,7 +138,7 @@ endmodule
 // The Tiny Tapeout module
 // =======================
 
-module tt_um_template (
+module my_design (
     input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
     output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
     /*   // The FPGA is based on TinyTapeout 3 which has no bidirectional I/Os (vs. TT6 for the ASIC).
@@ -462,8 +514,7 @@ logic FpgaPins_Fpga_CALC_valid_a1,
                            FpgaPins_Fpga_CALC_digit_a3 == 4'hD ? 8'b10100001 :
                            FpgaPins_Fpga_CALC_digit_a3 == 4'hE ? 8'b10000110 :
                                             8'b10001110;
-                        assign uo_out[7:0] = ~uo_out_temp[7:0];
-
+                     
                         
                      
                   
